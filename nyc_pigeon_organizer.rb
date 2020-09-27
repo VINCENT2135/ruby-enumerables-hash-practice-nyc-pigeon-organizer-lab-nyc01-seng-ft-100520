f5db05eeb9dd6dd997d0dd51412e2da499b83f8a
Vincent Ivos <vincentivos1@gmail.com>
2020-09-27 13:41:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
+  
+  people = []
+new_hash = {}
+
+pigeon_data.each do |key1,value1|
+  value1.each do |key2,value2|
+   value2.each do |value3|
+     
+    if !people.include?(value3)
+        people.push(value3)
+        new_hash[value3] = {}
+     end
+
+     if !new_hash[value3].key?(key1)
+         new_hash[value3].merge!({key1 => [] })
+     end
+     
+     new_hash[value3][key1] << key2.to_s
+     
+   end
+  end
+end
+  
+  
+  
   # write your code here!
 end
